cc8a157284bd22bec78af03a80921396c2a5534e
Chris McCauley <cdmccauley@gmail.com>
2018-11-15 19:22:39 -0700

    added some css, working on efficiency of data storage: want to skip post storage if ups not changed since last call

--- src/App.js
+++ src/App.js
@@ -6,33 +6,37 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      maxCORSReq: 4,
+      maxCORSReq: 2,
       corsReqCount: 0,
-      subreddit: 'politics',
+      subreddit: 'politics/new',
       listings: [],
       posts: [],
-      titles: null,
+      titles: 'loading...',
     }
 
     this.createCORSRequest = this.createCORSRequest.bind(this);
     this.sendCORSRequest = this.sendCORSRequest.bind(this);
 
-    this.storeTitles = this.storeTitles.bind(this);
+    this.parseListings = this.parseListings.bind(this);
+    this.getNewPosts = this.getNewPosts.bind(this);
   }
 
   componentDidMount() {
     this.sendCORSRequest();
-    setInterval(() => this.sendCORSRequest(), 30000);
+    // setInterval(() => this.sendCORSRequest(), 10000);
+    // setInterval(() => this.sendCORSRequest(), 15000);
+    // setInterval(() => this.sendCORSRequest(), 30000);
   }
 
   createCORSRequest(method, url) {
-    var createCORSReqXhr = new XMLHttpRequest();
+    let createCORSReqXhr = new XMLHttpRequest();
     if ("withCredentials" in createCORSReqXhr) {
       createCORSReqXhr.open(method, url, true);
     } else if (typeof XDomainRequest != "undefined") {
       // xhr = new XDomainRequest(); // related to IE
       // xhr.open(method, url);
       console.log('createCORSRequest function error: no XDomainRequest class defined.');
+      return null; // prevent continuation
     } else {
       createCORSReqXhr = null;
     }
@@ -61,7 +65,7 @@ class App extends Component {
         } else {
           sendCORSReqContext.setState({
             corsReqCount: 0
-          }, sendCORSReqContext.storeTitles());
+          }, sendCORSReqContext.parseListings());
         };
       });
     }
@@ -72,28 +76,79 @@ class App extends Component {
     sendCORSReqXhr.send();
   }
 
-  storeTitles() {
+  /*
+
+  don't want to keep spending so much on processing objects
+  restructure objects to enable using ids to check for dups
+  process:
+  call api, iterate through to find children that aren't in state
+  send children to be added to state if not in current set
+  then check differences between ups, if different overwrite ups
+
+  getListings, getNewPosts, getUpdatedUps, setState
+
+  */
+  getNewPosts(listings) {
+    
+  }
+
+  parseListings() {
     let tempPosts = [];
     Object.values(this.state.listings).forEach((listing) => {
       Object.values(listing.data.children).forEach((child) => {
-        tempPosts.push(child);
+        let post = {
+          id: child.data.id,
+          name: child.data.name,
+          created: child.data.created_utc,
+          title: child.data.title,
+          author: child.data.author,
+          comments: child.data.permalink,
+          ups: child.data.ups,
+          score: child.data.score,
+          domain: child.data.domain,
+          url: child.data.url,
+          thumb: child.data.thumbnail,
+          thumbHeight: child.data.thumbnail_height,
+          thumbWidth: child.data.thumbnail_width,
+        }
+        tempPosts.push(post);
       })
     });
-    tempPosts.sort((a, b) => { return b.data.ups - a.data.ups });
+    tempPosts.sort((a, b) => { return b.ups - a.ups });
+
+    // // DEBUG: testing area
+    // if (Object.keys(this.state.posts).length > 0) {
+    //   Object.values(this.state.listings).forEach((listing) => {
+    //     Object.values(listing.data.children).forEach((child) => {
+    //       Object.values(this.state.posts).forEach((post) => {
+    //         if (!Object.values(post).includes(child.data.id)) {
+    //           // unknown post detected
+    //           // triggers 2450 times ??
+    //           // have 50 posts when this happens
+    //           // iterating over every prop of object, recognizes 50 of them...
+    //           // too much iterating happening here
+    //           console.log('unknown post');
+    //         };
+    //       });
+    //     });
+    //   });
+    // };
+    // // DEBUG: end testing area
+
     this.setState({
       listings: [],
-      posts: [...tempPosts],
-      titles: tempPosts.map((post) => <li key={ post.data.id } >{ post.data.title + '...' + post.data.ups }</li>)
-    }, () => console.log(this.state)); // DEBUG: log updated state
+      posts: tempPosts,
+      titles: tempPosts.map((post) => <li key={ post.id } ><span className='ups'>{ post.ups }</span><a href={ post.url }>{ post.title }</a></li>)
+    }, () => console.log(this.state));
   }
 
   render() {
     return (
-      <div className="App">
-        <ul style={{ fontSize: '1.25em', listStyleType: 'none', paddingLeft: '0', margin: '0' }}>
+      <React.Fragment>
+        <ul>
           { this.state.titles }
         </ul>
-      </div>
+      </React.Fragment>
     );
   }
 }
